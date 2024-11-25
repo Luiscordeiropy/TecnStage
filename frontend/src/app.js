9daee9c9b22493b3af0f369f0782dc082179b8b4
Luiscordeiro <luiscordeiro2006@gmail.com>
2024-11-25 01:32:00 -0300

    atualização completa

---
+++ frontend/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './pages/HomePage'; // Importando a página Home
+
+const App = () => {
+  return (
+    <div>
+      <HomePage /> {/* Exibindo a página principal */}
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Componente principal
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // Ponto de entrada do React
+);
